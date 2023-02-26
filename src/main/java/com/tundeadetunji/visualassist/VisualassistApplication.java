7282f604de07bdfd614a90d4e96d6735d5108a91
Tunde Adetunji <tundeadetunji2017@gmail.com>
2023-02-26 12:25:15 +0100

    Initial commit

---
+++ src/main/java/com/tundeadetunji/visualassist/VisualassistApplication.java
@@ -0,0 +1,13 @@
+package com.tundeadetunji.visualassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualassistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tundeadetunji/visualassist/VisualassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tundeadetunji.visualassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
